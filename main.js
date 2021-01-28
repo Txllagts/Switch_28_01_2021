389d3690379e9a95f60dc5e2114c5e6c2e1219e0
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-01-28 18:58:27 -0500

    Switch_28_01_2021

---
+++ main.js
@@ -0,0 +1,14 @@
+let key = parseInt(prompt(`Ingrese un numero`));
+switch (key) {
+     case 1:
+        console.log(`Opcion uno`);
+        break;
+    case 2:
+        console.log(`Opcion dos`);
+        break;
+
+    default:
+        console.log(`Opcion incorrecta`);
+        break;
+}
+
